c796521f7377782e3d41c450bf8c185b7685d937
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2020-04-28 23:18:20 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,10 +9,8 @@ class Scraper
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
     
-    binding.pry
+    #binding.pry
   end
-  
-  Scraper.new.get_page
   
   def get_courses
     
